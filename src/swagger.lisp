d5b6bbc1a5ffa7f78c7299b961d930040801f5b9
Mark <evenson.not.org@gmail.com>
2017-01-15 11:18:30 +0100

    Add file missing from last commit

---
+++ src/swagger.lisp
@@ -0,0 +1,20 @@
+(in-package :stagger)
+
+(defun swagger-model (path &key (output-directory #p"/var/tmp"))
+  (#"main" 'io.swagger.codegen.SwaggerCodegen
+           (java:jnew-array-from-list "java.lang.String"
+                                      `("generate"
+                                        "-i" ,(namestring path)
+                                        "-l" "io.swagger.codegen.languages.SwaggerGenerator"
+                                        "-o" ,(namestring output-directory)))))
+
+(defun swagger/help ()
+  (#"main" 'io.swagger.codegen.SwaggerCodegen
+           (java:jnew-array-from-list "java.lang.String"
+                                      '("help"))))
+
+(defun swagger/languages ()
+  (#"main" 'io.swagger.codegen.SwaggerCodegen
+           (java:jnew-array-from-list "java.lang.String"
+                                      '("langs"))))
+
